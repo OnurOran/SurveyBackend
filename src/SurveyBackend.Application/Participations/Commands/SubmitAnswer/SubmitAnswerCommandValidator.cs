3c5a0cf4e5e6b1c3218627ba2ec4e9634b8f07bc
Onur Oran <oran.onur01@gmail.com>
2025-12-01 14:55:24 +0300

    text limit

--- src/SurveyBackend.Application/Participations/Commands/SubmitAnswer/SubmitAnswerCommandValidator.cs
+++ src/SurveyBackend.Application/Participations/Commands/SubmitAnswer/SubmitAnswerCommandValidator.cs
@@ -2,6 +2,8 @@ namespace SurveyBackend.Application.Participations.Commands.SubmitAnswer;
 
 public sealed class SubmitAnswerCommandValidator : AbstractValidator<SubmitAnswerCommand>
 {
+    private const int MaxTextAnswerLength = 2000;
+
     public SubmitAnswerCommandValidator()
     {
         RuleFor(x => x.ParticipationId)
@@ -13,5 +15,10 @@ public sealed class SubmitAnswerCommandValidator : AbstractValidator<SubmitAnswe
         RuleFor(x => x)
             .Must(x => !string.IsNullOrWhiteSpace(x.TextValue) || (x.OptionIds is not null && x.OptionIds.Any()))
             .WithMessage("At least one answer (text or options) must be provided.");
+
+        RuleFor(x => x.TextValue)
+            .MaximumLength(MaxTextAnswerLength)
+            .WithMessage($"Text answer cannot exceed {MaxTextAnswerLength} characters.")
+            .When(x => !string.IsNullOrWhiteSpace(x.TextValue));
     }
 }
